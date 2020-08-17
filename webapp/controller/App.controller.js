7343316300875a525f6183dd9a12ddf6e4a4ece6
Virendra Korishetti <codingvirendra@gmail.com>
2020-08-17 11:12:16 +0000

    First Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("SE.SMT_Employee.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"SE/SMT_Employee/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
